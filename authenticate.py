8fd2428d7a97f405eb29db88e679753d0a8f2aee
ASEDOV89 <a.sedov@x5.ru>
2025-01-29 19:03:15 +0300

    tests

--- authenticate.py
+++ authenticate.py
@@ -9,7 +9,7 @@ from database import get_db
 from schemas import UserCreate
 
 
-SECRET_KEY = "my_super_mega_puper_secret_key"
+SECRET_KEY = "my_super_puper_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -82,25 +82,3 @@ def get_current_user(request: Request, db: Session = Depends(get_db)):
         return user
     except JWTError:
         return None
-
-
-# def get_current_user(
-#     token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
-# ):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Неверные учетные данные",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         user = get_user(db, username)
-#         if user is None:
-#             raise credentials_exception
-#         return user
-#     except JWTError:
-#         raise credentials_exception
